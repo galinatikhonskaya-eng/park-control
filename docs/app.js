fc40ad89eb2a68d0a230c5448664e952b5f27a3d
galinatikhonskaya-eng <galinatikhonskaya@gmail.com>
2026-01-24 09:37:52 +0300

    Update app.js

--- docs/app.js
+++ docs/app.js
@@ -1,1 +1,6 @@
+if (window.Telegram?.WebApp) {
+  window.Telegram.WebApp.ready();  // Обязательно вызвать ready, если ещё не вызван
 
+  // Отключаем вертикальный свайп (закрытие/сворачивание при скролле вниз)
+  window.Telegram.WebApp.disableVerticalSwipes();  // или .setAllowVerticalSwipes(false) в некоторых версиях
+}
